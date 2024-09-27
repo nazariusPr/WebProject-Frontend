c389ccc602aafb1f2b2c6fa4864be993d44b1128
nazariusPr <protsnazar2004@gmail.com>
2024-09-27 23:02:36 +0300

    implement authentication routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoutes from "./routes/ProtectedRoutes";
+import AuthenticationRoutes from "./routes/AuthenticationRoutes";
 import RegistrationPage from "./pages/RegistrationPage";
 import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import VerifyEmailPage from "./pages/VerifyEmailPage";
-import DashboardPage from "./pages/DashBoardPage";
+import HomePage from "./pages/HomePage";
 import RoutesConstant from "./constants/client/RoutesConstant";
 import AuthProvider from "./context/AuthContext";
 
@@ -13,17 +14,20 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path={RoutesConstant.LOGIN} element={<LoginPage />} />
-          <Route
-            path={RoutesConstant.REGISTER}
-            element={<RegistrationPage />}
-          />
-          <Route
-            path={RoutesConstant.VERIFY_EMAIL}
-            element={<VerifyEmailPage />}
-          />
+          <Route element={<AuthenticationRoutes />}>
+            <Route path={RoutesConstant.LOGIN} element={<LoginPage />} />
+            <Route
+              path={RoutesConstant.REGISTER}
+              element={<RegistrationPage />}
+            />
+            <Route
+              path={RoutesConstant.VERIFY_EMAIL}
+              element={<VerifyEmailPage />}
+            />
+          </Route>
+
           <Route element={<ProtectedRoutes />}>
-            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path={RoutesConstant.HOME} element={<HomePage />} />
           </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
