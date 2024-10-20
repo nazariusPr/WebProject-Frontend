bf33dfa0729245c2726cb2302ef72e7cc6bf8b28
nazariusPr <protsnazar2004@gmail.com>
2024-10-20 21:58:58 +0300

    update dtos

--- src/types/Action.ts
+++ src/types/Action.ts
@@ -15,23 +15,28 @@ export enum ActionStatus {
   FINISHED,
 }
 
-export type ActionDto = {
-  id: string;
-  action_type: ActionType;
-  action_status: ActionStatus;
-  created_at: Date;
-  images: ImageDto[];
-};
-
-export enum ImageSize {
-  SMALL_IMAGE_SIZE = "256x256",
-  MEDIUM_IMAGE_SIZE = "512x512",
-  LARGE_IMAGE_SIZE = "1024x1024",
-}
-
 export type GenerateActionDto = {
   title: string;
   prompt: string;
   size: ImageSize;
   num_images: number;
 };
+
+export type ActionDto = {
+  id: string;
+  title: string;
+  action_type: ActionType;
+  action_status: ActionStatus;
+  created_at: Date;
+};
+
+export type DetailActionDto = {
+  images: ImageDto[];
+  action_request: GenerateActionDto;
+} & ActionDto;
+
+export enum ImageSize {
+  SMALL_IMAGE_SIZE = "256x256",
+  MEDIUM_IMAGE_SIZE = "512x512",
+  LARGE_IMAGE_SIZE = "1024x1024",
+}
